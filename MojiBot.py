436750f2400630590934fcc941453169adce749f
Fabricio Coronado <facosa99@gmail.com>
2023-09-07 22:29:07 -0600

    Simple responses are clean and basic commands are properly set. Time to start implementing more ambitious commands!

--- MojiBot.py
+++ MojiBot.py
@@ -3,18 +3,23 @@ import discord                          # Documentation: https://discordpy.readt
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
-from discord import app_commands
+import json
+from pathlib import Path
+
+# Local Libraries
 from NewMessage import Responses
 from NewMember import AutomaticWelcome
+from RockPaperScissors import RockPaperScissors
 
 load_dotenv()                           # Refresh enviroment
+current_dir = str(Path(__file__).resolve().parent).replace('\\', '/')   # Directory from which the program is executed/stored
 
 # intents are the permissions for the bot
 intents = discord.Intents.default()
 intents.members = True                  # Detect events related to a server member. For example, a new arrival or someone leaving the server
-intents.message_content = True
+intents.message_content = True          # Detects messages and their content, name is pretty self explanatory
 
-bot = commands.Bot(command_prefix="!", intents=intents)  # After setting the right permissions, send them
+bot = commands.Bot(command_prefix = ('Moji, ', 'moji, '), intents=intents, help_command=None)  # Set the prefix and the permissions.
 
 @bot.event       # This code block is executed once, after the Bot logins
 async def on_ready():
@@ -28,31 +33,125 @@ async def on_member_join(member):
 async def on_message(message):
     if f'{message.author}' == 'facosa99' and message.content.lower().startswith('moji, update commands'):
         # await bot.tree.sync()
-        await message.channel.send('"bot.tree.sync()" is temporaily disabled, masterr')
+        await message.channel.send('"bot.tree.sync()" is temporaily disabled, master')
         return
 
     if message.author == bot.user: return   # if current message is from Bot itself, ignore it and finish the function.
                                             # This prevents the bot from replying to itself and thus avoids undesirable loops.
 
-    await bot.process_commands(message)     # Check if received message is a command before trying regular replies
-    await Responses(message, bot)           # Now try regular replies
+    await Responses(message, bot)           # If you want simple responses to certain messages, set them in the associated JSON
+                                            # file and put this line. For more interactive responses (like repeating the message
+                                            # text or doing an operation) use commands, as those can receive arguments
+
+    await bot.process_commands(message)     # Check if received message is a command
+                                            # If your bot has no commands, then this line isnt necesary. Also you might want to change
+                                            # from "bot" to "client". Setting it as "bot" is only necesary for enabling commands
     return
 
-
 # -------------------------------------List of all available commands---------------------------------------------------
-@bot.command(name='say')
+@bot.command(name='say', help="Makes Moji say whatever you want")
 async def say(ctx, *arg):
     Text = " ".join(arg)
     await ctx.send(f"{Text}")
     await ctx.message.delete()
     return
 
-@bot.command(name='list', help="Gotta get this shit running")
-async def list(ctx):
-    await ctx.send(f"Here is the list of all available commands:\n"
-                   f"- (Insert list here)")
+@bot.command(name='help', help="Gotta get this shit running")
+async def help(ctx):
+    # Open the JSON file with the list of simple text replies
+    with open(current_dir + '/QuickResponses.json', 'r') as openfile:
+        ResponsesList = json.load(openfile)
+
+    Replies = ResponsesList # Dump the info of said file into a dictionary
+    ResponsesList.close()   # The info has been dumped into the dictionay, JSON can be closed now.
+
+    # Create a String to store our reply to the 'help' command
+    HelpReply = f'This is a list of some of the phrases that I respond to. ' \
+               f'I do not care about capitalization\n'
+
+    # Append the list of simple text replies into the reply
+    for key in Replies:        HelpList = HelpList + f'- {key}\n'
+
+    # Now append the more complex commands
+    HelpReply = HelpReply + "I can also respond to the following commands:\n"
+    for command in bot.commands:        HelpReply += f"- {command}\n"
+
+    # The reply is fully assembled, time to send it to the channel
+    await ctx.send(HelpReply)
+
     return
 
-#@bot.command(name="test_1", )
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Did you call my name? Type 'Moji, help' for a list of actions that i can perform!")
+
+@bot.event
+async def scream(ctx):
+    X = True;
+    if X:
+        await ctx.channel.send(f'Sorry, I cannot scream until master fixes the path of the audio files')
+        return         # This function is temporally disabled till it can be properly migrated to the server
+
+    if ctx.author.voice:    # This line checks if the user is in a voice channel
+        channel = ctx.author.voice.channel
+        # They are indeed in a channel, so first, answer the petition
+        await ctx.channel.send(f'Okie doki pokie!')
+        await asyncio.sleep(1)
+        # Now, lets connect to their channel
+        vc = await channel.connect()
+        vc.play(discord.FFmpegPCMAudio('AudioFiles/scream.mp3'), after=lambda e: print('done', e))
+        await asyncio.sleep(5)      # Dont disconnect until the audio finishes playing
+        await vc.disconnect()       # We finished our business, time to leave.
+
+        # player = vc.create_ffmpeg_player('scream.mp3', after=lambda: print('done'))
+        # player.start()
+        # while not player.is_done():
+        #    await asyncio.sleep(1)
+        # disconnect after the player has finished
+        # player.stop()
+        # await vc.disconnect()
+    else:
+        await ctx.channel.send(f'I dont see you in any voice channel')
+
+@bot.event
+async def sing(ctx):
+    X = True;
+    if X:
+        await ctx.channel.send(f'Sorry, I cannot scream until master fixes the path of the audio files')
+        return  # This function is temporally disabled till it can be properly migrated to the server
+    if ctx.author.voice:  # This line checks if the user is in a voice channel
+        # They are indeed in a channel, so first, answer the petition
+        channel = ctx.author.voice.channel
+        await ctx.channel.send(f'Okie doki pokie!')
+        await asyncio.sleep(1)
+        # Now, lets connect to their channel
+        vc = await channel.connect()
+        vc.play(discord.FFmpegPCMAudio('AudioFiles/IsabelleSong.mp3'), after=lambda e: print('done', e))
+        await asyncio.sleep(194)
+        await vc.disconnect()
+
+                # player = vc.create_ffmpeg_player('scream.mp3', after=lambda: print('done'))
+                # player.start()
+                # while not player.is_done():
+                #    await asyncio.sleep(1)
+                # disconnect after the player has finished
+                # player.stop()
+                # await vc.disconnect()
+    else:
+        await ctx.channel.send(f'I dont see you in any voice channel')
+
+@bot.event
+async def rock(ctx):
+    if f'{ctx.author}' == 'anthonyzf20':    await ctx.channel.send('Paper! I win!')
+    else:                                   await ctx.channel.send( RockPaperScissors('rock'))
+@bot.event
+async def paper(ctx):
+    if f'{ctx.author}' == 'anthonyzf20':    await ctx.channel.send('Scissors! I win!')
+    else:                                   await ctx.channel.send( RockPaperScissors('paper'))
+@bot.event
+async def scissors(ctx):
+    if f'{ctx.author}' == 'anthonyzf20':    await ctx.channel.send('Rock! I win!')
+    else:                                   await ctx.channel.send( RockPaperScissors('scissors'))
 
 bot.run(os.environ['BotToken']) # Bot's unique token, stored in the .env file within the same directory as the rest of the project
--- NewMessage.py
+++ NewMessage.py
@@ -1,18 +1,16 @@
 import asyncio
 import json
 from pathlib import Path
-from RockPaperScissors import RockPaperScissors
+
 import discord
 
 # Current directory
 current_dir = str(Path(__file__).resolve().parent).replace('\\', '/')
 
-async def Responses(message, client):
-
+async def Responses(message):
         # if f'{message.author}' == 'anthonyzf20':  # Reply to this user everytime the bot detects a new message from then
-        #    await message.channel.send('¿Quien?')
+        #   await message.channel.send('¿Quien?')  # Basically turn the bot into harasser-mode
         #   return
-
 
         # Open the JSON file with the list of simple text replies
         with open(current_dir + '/QuickResponses.json', 'r') as openfile:
@@ -20,6 +18,7 @@ async def Responses(message, client):
 
         # Dump the info of said file into a dictionary
         Replies = ResponsesList
+        ResponsesList.close()  # The info has been dumped into the dictionay, JSON can be closed now.
         Close = False
 
         # Check if we hit any of the JSON replies
@@ -29,75 +28,4 @@ async def Responses(message, client):
                 await message.channel.send(f'{Replies[key]}')
                 Close = True
             if Close: return    # If we hit the target phrase already, there is no need to run the rest of the for loop nor the rest of this function
-
-        # If we didnt trigger any simple text reply, is time to check for the slighty more interactive ones
-        if message.content.lower().startswith('moji, help'):
-            await message.channel.send(f'This is a list of some of the phrases that I respond to. '
-                                       f'I do not care about lowercase or uppercase')
-            HelpList = f''
-            for key in Replies:
-                HelpList = HelpList + f'- {key}\n'
-
-            # We append the phrases that arent included in the JSON file
-            HelpList = HelpList + f'- moji, scream\n' \
-                                  f'- moji, sing\n' \
-                                  f'- moji, rock\n' \
-                                  f'- moji, paper\n' \
-                                  f'- moji, scissors\n'
-            await message.channel.send(HelpList)
-
-        elif message.content.lower().startswith('moji, scream') and False:
-            if message.author.voice:  # This line checks if the user is in a voice channel
-                channel = message.author.voice.channel
-                # They are indeed in a channel, so first, answer the petition
-                await message.channel.send(f'Okie doki pokie!')
-                await asyncio.sleep(1)
-                # Now, lets connect to their channel
-                vc = await channel.connect()
-                vc.play(discord.FFmpegPCMAudio('AudioFiles/scream.mp3'), after=lambda e: print('done', e))
-                await asyncio.sleep(2)
-                await vc.disconnect()
-
-                # player = vc.create_ffmpeg_player('scream.mp3', after=lambda: print('done'))
-                # player.start()
-                # while not player.is_done():
-                #    await asyncio.sleep(1)
-                # disconnect after the player has finished
-                # player.stop()
-                # await vc.disconnect()
-            else:
-                await message.channel.send(f'I dont see you in any voice channel')
-        elif message.content.lower().startswith('moji, sing') and False:
-            if message.author.voice:  # This line checks if the user is in a voice channel
-                print(type(message.author.voice))
-                channel = message.author.voice.channel
-                # They are indeed in a channel, so first, answer the petition
-                await message.channel.send(f'Okie doki pokie!')
-                await asyncio.sleep(1)
-                # Now, lets connect to their channel
-                vc = await channel.connect()
-                vc.play(discord.FFmpegPCMAudio('AudioFiles/IsabelleSong.mp3'), after=lambda e: print('done', e))
-                await asyncio.sleep(194)
-                await vc.disconnect()
-                print(type(vc))
-
-                # player = vc.create_ffmpeg_player('scream.mp3', after=lambda: print('done'))
-                # player.start()
-                # while not player.is_done():
-                #    await asyncio.sleep(1)
-                # disconnect after the player has finished
-                # player.stop()
-                # await vc.disconnect()
-            else:
-                await message.channel.send(f'I dont see you in any voice channel')
-
-        elif message.content.lower().startswith('moji, rock'):
-            if f'{message.author}' == 'anthonyzf20':    await message.channel.send('Paper! I win!')
-            else:                                       await message.channel.send( RockPaperScissors('rock'))
-        elif message.content.lower().startswith('moji, paper'):
-            if f'{message.author}' == 'anthonyzf20':    await message.channel.send('Scissors! I win!')
-            else:                                       await message.channel.send( RockPaperScissors('paper'))
-        elif message.content.lower().startswith('moji, scissors'):
-            if f'{message.author}' == 'anthonyzf20':    await message.channel.send('Rock! I win!')
-            else:                                       await message.channel.send( RockPaperScissors('scissors'))
-        return
+        return                  # If we reach this far into the function, we haven't hit anything, close it.
